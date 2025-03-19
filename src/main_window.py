c8bd6f468f33531c82e43f48849d3d09f24de139
alphameo <pavel.2014.play@gmail.com>
2025-03-19 13:43:18 +0300

    feat(game_window/animation): falling animation on-click with movement blocking

--- src/main_window.py
+++ src/main_window.py
@@ -1,11 +1,13 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel
-from PyQt5.QtCore import QTimer, Qt
-from game import Cell, Game
+from PyQt5.QtCore import QEventLoop, Qt, QTimer
+from PyQt5.QtWidgets import QLabel, QMainWindow
+
+from game import Game
 from game_renderer import GameRenderer
 
 
 class MainWindow(QMainWindow):
     __game_window: GameRenderer
+    __timer: QTimer = QTimer()
 
     def __init__(self, width=300, height=500) -> None:
         super().__init__()
@@ -18,6 +20,9 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.label)
 
     def keyPressEvent(self, event) -> None:
+        if self.__timer.isActive():
+            return
+
         g: Game = self.__game_window.game
         if event.key() == Qt.Key.Key_Escape:
             g.deselect_cell()
@@ -36,42 +41,44 @@ class MainWindow(QMainWindow):
             self.upd()
         if event.key() == Qt.Key.Key_E:
             g.rotate_selection_counterclockwise()
-            self.upd()
+            self.animated_destruction(300)
         if event.key() == Qt.Key.Key_Q:
             g.rotate_selection_clockwise()
-            self.upd()
-        if event.key() == Qt.Key.Key_X:
-            print(self.__game_window.game.destroy_field_clusters())
-            self.upd()
-        if event.key() == Qt.Key.Key_C:
-            print(g.ascend_rows())
-            self.upd()
-        if event.key() == Qt.Key.Key_Z:
-            self.destruction_loop(0.2)
-        # if event.key() == Qt.Key.Key_L:
-        #     while True:
-        #         t.sleep(0.2)
-        #         print(1)
-
+            self.animated_destruction(300)
 
     # INFO: inner func instead of while loop because event loop blocking
-    def destruction_loop(self, time_sleep: float) -> None:
-        destroyed = self.__game_window.game.destroy_field_clusters()
+    def animated_destruction(self, time_sleep_ms: int) -> None:
         self.upd()
-        print(f"start: {destroyed=}")
+        destroyed = self.__game_window.game.destroy_field_clusters()
+        self.animate_ascending(destroyed, time_sleep_ms)
 
-        def update_and_check():
+
+        # def animation() -> None:
+        #     nonlocal destroyed
+        #     self.animate_ascending(destroyed, time_sleep_ms)
+        #     destroyed = self.__game_window.game.destroy_field_clusters()
+        #     if destroyed <= 0:
+        #         loop.quit()
+        #     else:
+        #         QTimer.singleShot(time_sleep_ms * 2, animation)
+        #
+        # loop = QEventLoop()
+        # QTimer.singleShot(time_sleep_ms * 2, animation)
+        # loop.exec_()
+        # def destruction_loop(self, time_sleep: int) -> None:
+
+    def animate_ascending(self, destroyed: int, time_sleep_ms: int) -> None:
+        def animation_frame():
             nonlocal destroyed
             destroyed -= self.__game_window.game.ascend_rows()
             self.upd()
-            print(destroyed)
 
             if destroyed <= 0:
-                self.timer.stop()
+                self.__timer.stop()
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(update_and_check)
-        self.timer.start(int(time_sleep * 1000))
+        self.__timer = QTimer()
+        self.__timer.timeout.connect(animation_frame)
+        self.__timer.start(time_sleep_ms)
 
     def upd(self):
         # print(self.__game_window.game.destroy_field_clusters())
